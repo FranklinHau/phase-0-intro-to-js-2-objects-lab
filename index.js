1df38217e0be06bc862bb0bcbf4404f0a0d77d6f
Franklin Hau <fhauchi@gmail.com>
2023-03-07 21:34:21 -0700

    Js 2 Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {
+    name : 'Franklin',
+    streetAddress : '373 Antioch'
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee,[key]: value,}
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee; 
+}function deleteFromEmployeeByKey(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    const newEmployee = employee;
+    newEmployee[key] = value;
+    return newEmployee;
+}
